6b700bc82564376606c8f090e248cfb3e43a3bbc
PatriciaRosalis <patriciarosalis@live.com.pt>
2019-07-15 00:32:03 +0100

    Updates

---
+++ precache-manifest.889745166e2dfb6db248f6f716f2e013.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30289aa4cbc06346ca0d0061a22865f3",
+    "url": "/AquaSalutis/index.html"
+  },
+  {
+    "revision": "bd0ca26bd6f30c2b4fb2",
+    "url": "/AquaSalutis/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "71c99142787e8e693c35",
+    "url": "/AquaSalutis/static/css/main.b9c2ad2e.chunk.css"
+  },
+  {
+    "revision": "bd0ca26bd6f30c2b4fb2",
+    "url": "/AquaSalutis/static/js/2.718d635d.chunk.js"
+  },
+  {
+    "revision": "71c99142787e8e693c35",
+    "url": "/AquaSalutis/static/js/main.54d54ad8.chunk.js"
+  },
+  {
+    "revision": "dc03e752f80d49398286",
+    "url": "/AquaSalutis/static/js/runtime~main.ca1e8a91.js"
+  }
+]);
